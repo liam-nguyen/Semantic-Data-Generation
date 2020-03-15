46fd4ac0845a8be71487e8bc9eb72d6ed366d7a3
Phuc Dang <dragon31194@gmail.com>
2020-03-15 12:42:52 -0700

    statistic hasYear, change statistic2018 to statistic, and minor fix

--- project_2/project2_maven/src/main/java/project2/Hospital/OntModel.java
+++ project_2/project2_maven/src/main/java/project2/Hospital/OntModel.java
@@ -11,12 +11,12 @@ import org.apache.jena.vocabulary.XSD;
  */
 public class OntModel {
     private static final String duURI = "http://www.ontologydesignpatterns.org/ont/dul/DUL.owl#";
-    public enum Classes {MedicareMetadata, Hospital, State, Nation, Statistics2018, Location, Country}
+    public enum Classes {MedicareMetadata, Hospital, State, Nation, Statistics, Location, Country}
     public enum Props {hasID, hasFacilityName, hasHospitalAverageSpending, isIDOf,
         isFacilityNameOf, hasEmergencyService, isEmergencyServiceOf, hasPhoneNumber, isHospitalAverageSpendingOf,
         hasScore, isScoreOf, hasRating, isRatingOf, hasLocation, isLocationOf, hasAddress, isAddressOf, hasZipcode,
         isZipcodeOf, hasCity, isCityOf, hasState, isStateOf, hasCountry, isCountryOf, hasType, isTypeOf, hasOwnership, 
-        isOwnershipOf}
+        isOwnershipOf, hasYear, isYearOf, hasStatistics, isStatisticsOf}
 
     public static String source = "https://data.medicare.gov/d/nrth-mfg3";
     public static String NS = source + "#";
@@ -36,7 +36,7 @@ public class OntModel {
         OntClass hospital = model.createClass(NS + Classes.Hospital);
         OntClass state = model.createClass(NS + Classes.State);
         OntClass nation = model.createClass(NS + Classes.Nation);
-        OntClass statistics2018 = model.createClass(NS + Classes.Statistics2018);
+        OntClass statistics = model.createClass(NS + Classes.Statistics);
         OntClass location = model.createClass(NS + Classes.Location);
         OntClass country = model.createClass(NS + Classes.Country);
         
@@ -44,13 +44,8 @@ public class OntModel {
         OntClass medicaremetadata = model.createClass(NS + Classes.MedicareMetadata);
         // Subclass relationship
         medicaremetadata.addSubClass(hospital);
-        hospital.addSuperClass(medicaremetadata);
-        
         medicaremetadata.addSubClass(state);
-        state.addSuperClass(medicaremetadata);
-        
         medicaremetadata.addSubClass(country);
-        country.addSuperClass(medicaremetadata);
         
 
         // Comments
@@ -108,20 +103,20 @@ public class OntModel {
         ObjectProperty hasHospitalAverageSpending = model.createObjectProperty(NS + Props.hasHospitalAverageSpending);
         hasHospitalAverageSpending.addComment("has a hospital's average spending per Beneficiary (MSPB) episodes in USD", "EN");
         hasHospitalAverageSpending.addRange(XSD.decimal);
-        hasHospitalAverageSpending.addDomain(statistics2018);
+        hasHospitalAverageSpending.addDomain(statistics);
         ObjectProperty isHospitalAverageSpendingOf = model.createObjectProperty(NS + Props.isHospitalAverageSpendingOf);
         isHospitalAverageSpendingOf.addComment("is a hospital's average spending per Beneficiary (MSPB) episodes in USD of", "EN");
         isHospitalAverageSpendingOf.addDomain(XSD.decimal);
-        isHospitalAverageSpendingOf.addRange(statistics2018);
+        isHospitalAverageSpendingOf.addRange(statistics);
 
         ObjectProperty hasScore = model.createObjectProperty(NS + Props.hasScore);
         hasScore.addComment("has a hospital's score", "EN");
         hasScore.addRange(XSD.integer);
-        hasScore.addDomain(statistics2018);
+        hasScore.addDomain(statistics);
         ObjectProperty isScoreOf = model.createObjectProperty(NS + Props.isScoreOf);
         isScoreOf.addComment("is a hospital's score", "EN");
-        isScoreOf.addDomain(statistics2018);
-        isScoreOf.addRange(XSD.integer);
+        isScoreOf.addDomain(XSD.integer);
+        isScoreOf.addRange(statistics);
 
         ObjectProperty hasRating = model.createObjectProperty(NS + Props.hasRating);
         hasRating.addComment("has a hospital's rating from 1-5", "EN");
@@ -129,13 +124,22 @@ public class OntModel {
         Literal ratingHigh = model.createTypedLiteral(5);
         DataRange ratingRange = model.createDataRange(model.createList(ratingLow,ratingHigh));
         hasRating.addRange(ratingRange);
-        hasRating.addDomain(statistics2018);
+        hasRating.addDomain(statistics);
         ObjectProperty isRatingOf = model.createObjectProperty(NS + Props.isRatingOf);
         isRatingOf.addComment("is a hospital's rating from 1-5", "EN");
         isRatingOf.addDomain(ratingRange);
-        isRatingOf.addRange(statistics2018);
+        isRatingOf.addRange(statistics);
         
         // D.Phuc's
+        ObjectProperty hasYear = model.createObjectProperty(NS + Props.hasYear);
+        hasYear.addComment("has a statistic's year", "EN");
+        hasYear.addRange(XSD.xstring);
+        hasYear.addDomain(statistics);
+        ObjectProperty isYearOf = model.createObjectProperty(NS + Props.isYearOf);
+        isYearOf.addComment("is a statistic's year", "EN");
+        isYearOf.addRange(statistics);
+        isYearOf.addDomain(XSD.xstring);
+        
         ObjectProperty hasLocation = model.createObjectProperty(NS + Props.hasLocation);
         hasLocation.addComment("has a hospital's location", "EN");
         hasLocation.addRange(location);
@@ -146,47 +150,47 @@ public class OntModel {
         isLocationOf.addDomain(location);
         
         ObjectProperty hasAddress = model.createObjectProperty(NS + Props.hasAddress);
-        hasAddress.addComment("has a location's address", "EN");
+        hasAddress.addComment("has a hospital location's address", "EN");
         hasAddress.addRange(XSD.xstring);
         hasAddress.addDomain(location);
         ObjectProperty isAddressOf = model.createObjectProperty(NS + Props.isAddressOf);
-        isAddressOf.addComment("is a location's address", "EN");
+        isAddressOf.addComment("is a hospital location's address", "EN");
         isAddressOf.addRange(location);
         isAddressOf.addDomain(XSD.xstring);
         
         ObjectProperty hasZipcode = model.createObjectProperty(NS + Props.hasZipcode);
-        hasZipcode.addComment("has a location's Zipcode", "EN");
+        hasZipcode.addComment("has a hospital location's Zipcode", "EN");
         hasZipcode.addRange(XSD.xstring);
         hasZipcode.addDomain(location);
         ObjectProperty isZipcodeOf = model.createObjectProperty(NS + Props.isZipcodeOf);
-        isZipcodeOf.addComment("is a location's Zipcode", "EN");
+        isZipcodeOf.addComment("is a hospital location's Zipcode", "EN");
         isZipcodeOf.addRange(location);
         isZipcodeOf.addDomain(XSD.xstring);
         
         ObjectProperty hasCity = model.createObjectProperty(NS + Props.hasCity);
-        hasCity.addComment("has a location's city", "EN");
+        hasCity.addComment("has a hospital location's city", "EN");
         hasCity.addRange(XSD.xstring);
         hasCity.addDomain(location);
         ObjectProperty isCityOf = model.createObjectProperty(NS + Props.isCityOf);
-        isCityOf.addComment("is a location's city", "EN");
+        isCityOf.addComment("is a hospital location's city", "EN");
         isCityOf.addRange(location);
         isCityOf.addDomain(XSD.xstring);
         
         ObjectProperty hasCountry = model.createObjectProperty(NS + Props.hasCountry);
-        hasCountry.addComment("has a location's country", "EN");
+        hasCountry.addComment("has a hospital location's country", "EN");
         hasCountry.addRange(country);
         hasCountry.addDomain(location);
         ObjectProperty isCountryOf = model.createObjectProperty(NS + Props.isCountryOf);
-        isCountryOf.addComment("is a location's country", "EN");
+        isCountryOf.addComment("is a hospital location's country", "EN");
         isCountryOf.addRange(location);
         isCountryOf.addDomain(country);
         
         ObjectProperty hasState = model.createObjectProperty(NS + Props.hasState);
-        hasState.addComment("has a location's state", "EN");
+        hasState.addComment("has a hospital location's state", "EN");
         hasState.addRange(state);
         hasState.addDomain(location);
         ObjectProperty isStateOf = model.createObjectProperty(NS + Props.isStateOf);
-        isStateOf.addComment("is a location's state", "EN");
+        isStateOf.addComment("is a hospital location's state", "EN");
         isStateOf.addRange(location);
         isStateOf.addDomain(state);
         
@@ -200,13 +204,22 @@ public class OntModel {
         isTypeOf.addDomain(XSD.xstring);
         
         ObjectProperty hasOwnership = model.createObjectProperty(NS + Props.hasOwnership);
-        hasOwnership.addComment("has a hospital's type", "EN");
+        hasOwnership.addComment("has a hospital's ownership", "EN");
         hasOwnership.addRange(XSD.xstring);
         hasOwnership.addDomain(hospital);
         ObjectProperty isOwnershipOf = model.createObjectProperty(NS + Props.isOwnershipOf);
-        isOwnershipOf.addComment("is a hospital's type", "EN");
+        isOwnershipOf.addComment("is a hospital's ownership", "EN");
         isOwnershipOf.addRange(hospital);
         isOwnershipOf.addDomain(XSD.xstring);
+        
+        ObjectProperty hasStatistics = model.createObjectProperty(NS + Props.hasStatistics);
+        hasStatistics.addComment("has MedicareMetadata's statistics", "EN");
+        hasStatistics.addRange(statistics);
+        hasStatistics.addDomain(medicaremetadata);
+        ObjectProperty isStatisticsOf = model.createObjectProperty(NS + Props.isStatisticsOf);
+        isStatisticsOf.addComment("is MedicareMetadata's statistics", "EN");
+        isStatisticsOf.addRange(medicaremetadata);
+        isStatisticsOf.addDomain(statistics);
     }
 
     public static void main(String[] args) {
