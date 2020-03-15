617be4c5630c3d69f2092548b7245594d87dcec3
Phuc Dang <dragon31194@gmail.com>
2020-03-15 11:55:59 -0700

    Props of type, ownership and classes of medicaremetada, subclass relationship of state, country, hospital

--- project_2/project2_maven/src/main/java/project2/Hospital/OntModel.java
+++ project_2/project2_maven/src/main/java/project2/Hospital/OntModel.java
@@ -11,11 +11,12 @@ import org.apache.jena.vocabulary.XSD;
  */
 public class OntModel {
     private static final String duURI = "http://www.ontologydesignpatterns.org/ont/dul/DUL.owl#";
-    public enum Classes {Hospital, State, Nation, Statistics2018, Location, Country}
+    public enum Classes {MedicareMetadata, Hospital, State, Nation, Statistics2018, Location, Country}
     public enum Props {hasID, hasFacilityName, hasHospitalAverageSpending, isIDOf,
         isFacilityNameOf, hasEmergencyService, isEmergencyServiceOf, hasPhoneNumber, isHospitalAverageSpendingOf,
         hasScore, isScoreOf, hasRating, isRatingOf, hasLocation, isLocationOf, hasAddress, isAddressOf, hasZipcode,
-        isZipcodeOf, hasCity, isCityOf, hasState, isStateOf, hasCountry, isCountryOf}
+        isZipcodeOf, hasCity, isCityOf, hasState, isStateOf, hasCountry, isCountryOf, hasType, isTypeOf, hasOwnership, 
+        isOwnershipOf}
 
     public static String source = "https://data.medicare.gov/d/nrth-mfg3";
     public static String NS = source + "#";
@@ -38,6 +39,19 @@ public class OntModel {
         OntClass statistics2018 = model.createClass(NS + Classes.Statistics2018);
         OntClass location = model.createClass(NS + Classes.Location);
         OntClass country = model.createClass(NS + Classes.Country);
+        
+        // D.Phuc's
+        OntClass medicaremetadata = model.createClass(NS + Classes.MedicareMetadata);
+        // Subclass relationship
+        medicaremetadata.addSubClass(hospital);
+        hospital.addSuperClass(medicaremetadata);
+        
+        medicaremetadata.addSubClass(state);
+        state.addSuperClass(medicaremetadata);
+        
+        medicaremetadata.addSubClass(country);
+        country.addSuperClass(medicaremetadata);
+        
 
         // Comments
         nation.addComment("A large body of people united by common descent, history, culture, or language, inhabiting a particular country or territory.", "EN");
@@ -120,6 +134,8 @@ public class OntModel {
         isRatingOf.addComment("is a hospital's rating from 1-5", "EN");
         isRatingOf.addDomain(ratingRange);
         isRatingOf.addRange(statistics2018);
+        
+        // D.Phuc's
         ObjectProperty hasLocation = model.createObjectProperty(NS + Props.hasLocation);
         hasLocation.addComment("has a hospital's location", "EN");
         hasLocation.addRange(location);
@@ -173,6 +189,24 @@ public class OntModel {
         isStateOf.addComment("is a location's state", "EN");
         isStateOf.addRange(location);
         isStateOf.addDomain(state);
+        
+        ObjectProperty hasType = model.createObjectProperty(NS + Props.hasType);
+        hasType.addComment("has a hospital's type", "EN");
+        hasType.addRange(XSD.xstring);
+        hasType.addDomain(hospital);
+        ObjectProperty isTypeOf = model.createObjectProperty(NS + Props.isTypeOf);
+        isTypeOf.addComment("is a hospital's type", "EN");
+        isTypeOf.addRange(hospital);
+        isTypeOf.addDomain(XSD.xstring);
+        
+        ObjectProperty hasOwnership = model.createObjectProperty(NS + Props.hasOwnership);
+        hasOwnership.addComment("has a hospital's type", "EN");
+        hasOwnership.addRange(XSD.xstring);
+        hasOwnership.addDomain(hospital);
+        ObjectProperty isOwnershipOf = model.createObjectProperty(NS + Props.isOwnershipOf);
+        isOwnershipOf.addComment("is a hospital's type", "EN");
+        isOwnershipOf.addRange(hospital);
+        isOwnershipOf.addDomain(XSD.xstring);
     }
 
     public static void main(String[] args) {
