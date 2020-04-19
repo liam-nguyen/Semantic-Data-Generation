a20c3bde96ae51c289fb680f2fc88a128d4040f9
Loc Hinh <lhinh3431@gmail.com>
2020-04-19 13:57:43 -0700

    added files to assist in file uploading. needs to be edited to run properly.

---
+++ project_2_and_3/SpringBootWebService/src/main/java/team/three/storage/StorageException.java
@@ -0,0 +1,12 @@
+package team.three.storage;
+
+public class StorageException extends RuntimeException {
+
+	public StorageException(String message) {
+		super(message);
+	}
+
+	public StorageException(String message, Throwable cause) {
+		super(message, cause);
+	}
+}
